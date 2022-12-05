1db633379f7ce00ce7851c526e4aadc0336dc693
Angelo Munhoz <angelomca09@gmail.com>
2022-12-05 09:52:19 -0300

    Lendo os inputs da Calculadora

--- index.js
+++ index.js
@@ -1,3 +1,73 @@
 function readInput(element) {
-  console.log(element);
+  let input = element.innerText;
+  switch (input) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      insertNumber(input);
+      break;
+    case "+":
+    case "-":
+    case "/":
+    case "x":
+      insertOperator(input);
+      break;
+    case "=":
+      calculate();
+      break;
+    case "C":
+      cancel();
+      break;
+    case "DEL":
+      deleteInsertion();
+      break;
+    case ".":
+      insertFloat();
+      break;
+    case "+/-":
+      negative();
+      break;
+  }
+}
+
+function insertNumber(number) {
+  console.log("insertNumber: ", number);
+  //TODO
+}
+
+function insertOperator(operator) {
+  console.log("insertOperator: ", operator);
+  //TODO
+}
+
+function calculate() {
+  console.log("calculate");
+  //TODO
+}
+
+function cancel() {
+  console.log("cancel");
+  //TODO
+}
+
+function deleteInsertion() {
+  console.log("delete");
+  //TODO
+}
+
+function insertFloat() {
+  console.log("insertFloat");
+  //TODO
+}
+
+function negative() {
+  console.log("negative");
+  //TODO
 }
